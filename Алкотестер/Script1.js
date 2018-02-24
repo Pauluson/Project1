72209cf0a89ab958a4d5166f81c4e177a071c8bc
Pavel123 <pasha.sobko.95@mail.ru>
2018-02-24 12:39:09 +0300

    commit1

---
+++ Алкотестер/Script1.js
@@ -0,0 +1,63 @@
+﻿
+let flag = true;
+do {
+    let a = 10 * Math.random();
+    a = Math.round(a);
+    let b = 10 * Math.random();
+    b = Math.round(b);
+    let c = 4 * Math.random();
+    c = Math.ceil(c);
+    if (c == 1) {
+        let d = a + b;
+        alert(`${a} + ${b} `);
+        let st = prompt("введите ответ");
+        if (st == d) {
+            alert("Можно наливать");
+        }
+        else {
+            alert("Хватить пить!");
+            flag = false;
+        }
+
+    }
+    else if (c == 2) {
+        let d = a - b;
+        alert(`${a} - ${b} `);
+        let st = prompt("введите ответ");
+        if (st == d) {
+            alert("Можно наливать");
+        }
+        else {
+            alert("Хватит пить!");
+            flag = false;
+        }
+    }
+    else if (c == 3) {
+        let d = a * b;
+        alert(`${a} * ${b} `);
+        let st = prompt("введите ответ");
+        if (st == d) {
+            alert("Можно наливать");
+        }
+        else {
+            alert("Хватит пить!");
+            flag = false;
+        }
+    }
+    else {
+        while (b == 0) {
+            b = 10 * Math.random();
+            b = Math.round(b);
+        }
+        let d = a / b;
+        alert(`${a} / ${b} `);
+        let st = prompt("введите ответ");
+        if (st == d) {
+            alert("Можно наливать");
+        }
+        else {
+            alert("НХватит пить!");
+            flag = false;
+        }
+    }
+} while (flag);
